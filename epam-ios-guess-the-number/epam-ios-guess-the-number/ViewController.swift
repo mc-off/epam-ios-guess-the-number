d5223947d2125b5bb670f0281a514c9e2582ff64
mc_off <makov.tm@gmail.com>
2020-01-23 11:05:01 +0300

    Initial commit with simple view

---
+++ epam-ios-guess-the-number/epam-ios-guess-the-number/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  epam-ios-guess-the-number
+//
+//  Created by Артем Маков on 23/01/2020.
+//  Copyright © 2020 Артем Маков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ epam-ios-guess-the-number/epam-ios-guess-the-numberTests/epam_ios_guess_the_numberTests.swift
@@ -0,0 +1,34 @@
+//
+//  epam_ios_guess_the_numberTests.swift
+//  epam-ios-guess-the-numberTests
+//
+//  Created by Артем Маков on 23/01/2020.
+//  Copyright © 2020 Артем Маков. All rights reserved.
+//
+
+import XCTest
+@testable import epam_ios_guess_the_number
+
+class epam_ios_guess_the_numberTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
